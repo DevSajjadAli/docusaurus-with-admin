5dd9dc3ef223d418f172d66bef60fa2ff8720478
Sajjad Ali Babar <jacksbrown443@gmail.com>
2026-01-31 20:44:05 +0500

    Change production and base URLs in config
    Updated the production URL and base URL for deployment.

--- docusaurus.config.ts
+++ docusaurus.config.ts
@@ -17,10 +17,10 @@ const config: Config = {
   },
 
   // Set the production url of your site here
-  url: 'https://adminpandoc.sajjadalibabar.live',
+  url: 'https://devsajjadali.github.io',
   // Set the /<baseUrl>/ pathname under which your site is served
   // For GitHub pages deployment, it is often '/<projectName>/'
-  baseUrl: '/',
+  baseUrl: '/docusaurus-with-admin/',
 
   // GitHub pages deployment config.
   // If you aren't using GitHub pages, you don't need these.
